de719428e8dc615b94f93083675bcce1517b204c
HangJau <48112437+HangJau@users.noreply.github.com>
2021-12-04 15:40:21 +0800

    提交新的版本1.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r",encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pywchat",
-    version="1.0.0",
+    version="1.3.0",
     author="Hangjau",
     author_email="hangjau818@gmail.com",
     description="Custom WeChat push information database",
