0bb48eaf32ebdcdf7059e45fd12852e0538dcb9a
fanei <faneizn@gmail.com>
2024-04-15 19:45:22 +0800

    Update chatapi.py
    add GET_USERID_EMAIL

--- pywchat/chatapi.py
+++ pywchat/chatapi.py
@@ -3,5 +3,6 @@ chat_api = {
     'MESSAGE_SEND': '/cgi-bin/message/send?access_token={}',
     'MEDIA_UPLOAD': '/cgi-bin/media/upload?access_token={}&type={}',
     "IMG_UPLOAD": '/cgi-bin/media/uploadimg?access_token={}',
-    "GET_USERS": "/cgi-bin/user/simplelist"
+    "GET_USERS": "/cgi-bin/user/simplelist"，
+    "GET_USERID_EMAIL": "/cgi-bin/user/get_userid_by_email"
 }
