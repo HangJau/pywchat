5d759cd6109423c9994b91dc6dfbed702e82b4cc
HangJau <865360832@qq.com>
2024-04-16 21:06:05 +0800

    modify 版本更新至1.5.0及修改项目地址为最新地址

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r",encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pywchat",
-    version="1.4.0",
+    version="1.5.0",
     author="Hangjau",
     author_email="hangjau818@gmail.com",
     description="Custom WeChat push information database",
     long_description=long_description,
     long_description_content_type="text/markdown",
     platforms='any',
-    url="https://github.com/HangJau/pychatbot",
+    url="https://github.com/HangJau/pywchat",
     packages=setuptools.find_packages(),
     install_requires=[
         "requests>=2.22.0"
